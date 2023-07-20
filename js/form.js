06037520f0fb32e3baded464fe16ead43ded1b2b
Hyeong <hyeongyu36@gmail.com>
2023-07-20 13:14:29 +0900

    230720-study-form

---
+++ js/form.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+  var check = document.querySelector("#shippingInfo"); //체크박스에 아이디를 담음.
+  check.addEventListener("click", function () {
+    if (check.checked == true) {
+      document.querySelector("#shippingName").value =
+        document.querySelector("#billingName").value;
+      document.querySelector("#shippingTel").value =
+        document.querySelector("#billingTel").value;
+      document.querySelector("#shippingAddr").value =
+        document.querySelector("#billingAddr").value;
+    } else {
+      document.querySelector("#shippingName").value = "";
+      document.querySelector("#shippingTel").value = "";
+      document.querySelector("#shippingAddr").value = "";
+    }
+  });
+};
